527e5f1a2bd1b011d5f95b226eba16e1fd2bd226
Tomislav Miklenic <tmiklenic@gmail.com>
2023-06-30 21:18:17 +0200

    small fix

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Results = ({userData, repoData}) => {
     return (
         <div>
-            <img src={userData.avatar_url} height='200' width='200'/>
+            <img src={userData.avatar_url} height='200' width='200' alt=''/>
             <h1>{userData.name}</h1>
             <p>Bio: {userData.bio}</p>
             <p>Location: {userData.location}</p>
